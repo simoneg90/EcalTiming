68db506e1da7fac4b639434ead41c363f86da6e4
Shervin <shervin@cern.ch>
2015-04-01 14:03:53 +0200

    revolution with sum, sum2 and num instead of mean, rms, etc.

--- EcalTiming/interface/EcalCrystalTimingCalibration.h
+++ EcalTiming/interface/EcalCrystalTimingCalibration.h
@@ -2,184 +2,228 @@
 
 #include <vector>
 
+/** \class EcalCrystalTimingCalibration EcalCrystalTimingCalibration.cc EcalCrystalTimingCalibration.cc
+ *
+ * Description: add a description here
+ * This class contains all the timing information for a single crystal
+ */
+
+
 class EcalCrystalTimingCalibration
 {
-  public:
-    float mean;
-    float meanE; // error on the mean
-    float rms;
-    float stdDev;
-    float totalChi2;
-    std::vector<EcalTimingEvent> timingEvents;
-    std::vector<EcalTimingEvent>::iterator maxChi2Itr;
+public:
+	DetId _detId; ///< detId of the channel
 
-    EcalCrystalTimingCalibration() :
-      mean(-1),
-      meanE(-1),
-      rms(-1),
-      totalChi2(-1),
-      useWeightedMean(true)
-    {
-    }
-    
-    EcalCrystalTimingCalibration(bool weightMean) :
-      mean(-1),
-      meanE(-1),
-      rms(-1),
-      totalChi2(-1),
-      useWeightedMean(weightMean)
-    {
-    }
-      
-    EcalCrystalTimingCalibration(float m, float me, float r, float tc, std::vector<EcalTimingEvent> te) :
-      mean(m),
-      meanE(me),
-      rms(r),
-      totalChi2(tc)
-    {
-      timingEvents = te;
-    }
 
-    EcalCrystalTimingCalibration(float m, float me, float r, float tc, std::vector<EcalTimingEvent> te, bool wm) :
-      mean(m),
-      meanE(me),
-      rms(r),
-      totalChi2(tc),
-      useWeightedMean(wm)
-    {
-      timingEvents = te;
-    }
+private:
 
-    bool insertEvent(float amp, float t, float sigmaT, bool ee)
-    {
-      if(sigmaT > 0) // throw away events with zero or negative errors
-      {
-        timingEvents.push_back(EcalTimingEvent(amp,t,sigmaT,ee));
-        updateChi2();
-        return true;
-      }
-      else
-        return false;
-    }
+	float _sum; ///< scalar sum of the time of each timingEvent
+	float _sum2; ///< scalar sum of the square of the time of each timingEvent
+	float _num; ///< number of timingEvents;
 
-    bool insertEvent(EcalTimingEvent te)
-    {
-      if(te.sigmaTime > 0)
-      {
-        timingEvents.push_back(te);
-        updateChi2();
-        return true;
-      }
-      else
-        return false;
-    }
+	std::vector<EcalTimingEvent> timingEvents; ///< vector containing  all the events for this crystal
+	std::vector<EcalTimingEvent>::iterator maxChi2Itr;
 
-    int filterOutliers(float threshold = 0.5)
-    {
-      int numPointsErased = 0;
-      while(timingEvents.size() > 4)
-      {
-        updateChi2();
-        float oldMean = mean;
-        // Erase largest chi2 event
-        EcalTimingEvent toRemove = *maxChi2Itr;
-        timingEvents.erase(maxChi2Itr);
-        //Calculate new mean/error
-        updateChi2();
-        //Compare to old mean and break if |(newMean-oldMean)| < newSigma
-        //TODO: study acceptance threshold
-        if(fabs(mean-oldMean) < threshold*meanE)
-        {
-          insertEvent(toRemove);
-          break;
-        }
-        else
-        {
-          numPointsErased++;
-        }
-      }
-      return numPointsErased;
-    }
 
-  private:
-    bool useWeightedMean;
+public:
+	/// default constructor
+	EcalCrystalTimingCalibration(bool weightMean = true) :
+		_detId(), _sum(0), _sum2(0), _num(0)
+		//totalChi2(-1),
+		//useWeightedMean(weightMean)
+	{
+	}
 
-    void updateChi2() // update individual, total, maxChi2s
-    {
-      if(useWeightedMean)
-        updateChi2Weighted();
-      else
-        updateChi2Unweighted();
-    }
+	inline float mean()
+	{
+		return _sum / _num;
+	}; ///< average time (mean of the time distribution)
+	inline float stdDev()   ///< standard deviation of the time distribution
+	{
+		float mean_ = mean();
+		return sqrt(_sum2 / _num - mean_ * mean_);
+	}
+	inline float meanError()
+	{
+		return stdDev() / sqrt(_num);
+	};
 
-    void updateChi2Weighted()
-    {
-      updateMeanWeighted();
-      float chi2 = 0;
-      maxChi2Itr = timingEvents.begin();
-      for(std::vector<EcalTimingEvent>::iterator itr = timingEvents.begin();
-          itr != timingEvents.end(); ++itr)
-      {
-        float singleChi = (itr->time-mean)/itr->sigmaTime;
-        itr->chi2 = singleChi*singleChi;
-        chi2+=singleChi*singleChi;
-        if(itr->chi2 > maxChi2Itr->chi2)
-          maxChi2Itr = itr;
-      }
-      totalChi2 = chi2;
-    }
+	bool operator<( EcalCrystalTimingCalibration& rhs)
+	{
+		if(_detId < rhs._detId) return true;
+		else return false;
+	}
+	//float totalChi2;
 
-    void updateChi2Unweighted()
-    {
-      updateMeanUnweighted();
-      float chi2 = 0;
-      maxChi2Itr = timingEvents.begin();
-      for(std::vector<EcalTimingEvent>::iterator itr = timingEvents.begin();
-          itr != timingEvents.end(); ++itr)
-      {
-        float singleChi = (itr->time-mean);
-        itr->chi2 = singleChi*singleChi;
-        chi2+=singleChi*singleChi;
-        if(itr->chi2 > maxChi2Itr->chi2)
-          maxChi2Itr = itr;
-      }
-      totalChi2 = chi2;
-    }
 
-    void updateMeanWeighted()
-    {
-      float meanTmp = 0;
-      float mean2Tmp = 0;
-      float sigmaTmp = 0;
-      for(std::vector<EcalTimingEvent>::const_iterator itr = timingEvents.begin();
-          itr != timingEvents.end(); ++itr)
-      {
-        float sigmaT2 = itr->sigmaTime;
-        sigmaT2*=sigmaT2;
-        sigmaTmp+=1/(sigmaT2);
-        meanTmp+=(itr->time)/(sigmaT2);
-        mean2Tmp+=((itr->time)*(itr->time))/(sigmaT2);
-      }
-      meanE = sqrt(1/sigmaTmp);
-      mean = meanTmp/sigmaTmp;
-      rms = sqrt(mean2Tmp/sigmaTmp);
-      stdDev = sqrt(rms*rms-mean*mean);
-    }
+	/* EcalCrystalTimingCalibration(float m, float me, float r, float tc, std::vector<EcalTimingEvent> te) : */
+	/* 	mean(m), */
+	/* 	meanE(me), */
+	/* 	rms(r), */
+	/* 	totalChi2(tc) */
+	/* { */
+	/* 	timingEvents = te; */
+	/* } */
 
-    void updateMeanUnweighted()
-    {
-      float meanTmp = 0;
-      float mean2Tmp = 0;
-      for(std::vector<EcalTimingEvent>::const_iterator itr = timingEvents.begin();
-          itr != timingEvents.end(); ++itr)
-      {
-        meanTmp+=itr->time;
-        mean2Tmp+=(itr->time)*(itr->time);
-      }
-      mean = meanTmp/timingEvents.size();
-      rms = sqrt(mean2Tmp/timingEvents.size());
-      stdDev = sqrt(rms*rms-mean*mean);
-      meanE = stdDev/sqrt(timingEvents.size()); // stdDev/sqrt(n)
-    }
-    
+	/* EcalCrystalTimingCalibration(float m, float me, float r, float tc, std::vector<EcalTimingEvent> te, bool wm) : */
+	/* 	mean(m), */
+	/* 	meanE(me), */
+	/* 	rms(r), */
+	/* 	totalChi2(tc), */
+	/* 	useWeightedMean(wm) */
+	/* { */
+	/* 	timingEvents = te; */
+	/* } */
+
+	/* bool insertEvent(float amp, float t, float sigmaT, bool ee) */
+	/* { */
+	/* 	if(sigmaT > 0) // throw away events with zero or negative errors */
+	/* 	{ */
+	/* 		timingEvents.push_back(EcalTimingEvent(amp, t, sigmaT, ee)); */
+	/* 		updateChi2(); */
+	/* 		return true; */
+	/* 	} */
+	/* 	else { */
+	/* 		return false; */
+	/* 	} */
+	/* } */
+
+	/// add new event for this crystal
+	inline bool add(EcalTimingEvent te_)
+	{
+		return insertEvent(te_);
+	}
+private:
+	/// \todo weighted average by timeError
+	bool insertEvent(EcalTimingEvent te_)
+	{
+		if(te_._sigmaTime > 0) {
+			_sum += te_._amplitude;
+			_sum2 += te_._amplitude * te_._amplitude;
+			_num++;
+			timingEvents.push_back(te_);
+			//updateChi2();
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	/* int filterOutliers(float threshold = 0.5) */
+	/* { */
+	/* 	int numPointsErased = 0; */
+
+	/* 	while(timingEvents.size() > 4) { */
+	/* 		updateChi2(); */
+	/* 		float oldMean = mean; */
+	/* 		// Erase largest chi2 event */
+	/* 		EcalTimingEvent toRemove = *maxChi2Itr; */
+	/* 		timingEvents.erase(maxChi2Itr); */
+	/* 		//Calculate new mean/error */
+	/* 		updateChi2(); */
+
+	/* 		//Compare to old mean and break if |(newMean-oldMean)| < newSigma */
+	/* 		//TODO: study acceptance threshold */
+	/* 		if(fabs(mean - oldMean) < threshold * meanE) { */
+	/* 			insertEvent(toRemove); */
+	/* 			break; */
+	/* 		} else { */
+	/* 			numPointsErased++; */
+	/* 		} */
+	/* 	} */
+
+	/* 	return numPointsErased; */
+	/* } */
+
+private:
+	/* bool useWeightedMean; */
+
+	/* //calculate chi2: assume a gaussian distribution */
+	/* void updateChi2() // update individual, total, maxChi2s */
+	/* { */
+	/* 	if(useWeightedMean) { */
+	/* 		updateChi2Weighted(); */
+	/* 	} else { */
+	/* 		updateChi2Unweighted(); */
+	/* 	} */
+	/* } */
+
+	/* void updateChi2Weighted() */
+	/* { */
+	/* 	updateMeanWeighted(); */
+	/* 	float chi2 = 0; */
+	/* 	maxChi2Itr = timingEvents.begin(); */
+
+	/* 	for(std::vector<EcalTimingEvent>::iterator itr = timingEvents.begin(); */
+	/* 	        itr != timingEvents.end(); ++itr) { */
+	/* 		float singleChi = (itr->time - mean) / itr->sigmaTime; */
+	/* 		itr->chi2 = singleChi * singleChi; */
+	/* 		chi2 += singleChi * singleChi; */
+
+	/* 		if(itr->chi2 > maxChi2Itr->chi2) { */
+	/* 			maxChi2Itr = itr; */
+	/* 		} */
+	/* 	} */
+
+	/* 	totalChi2 = chi2; */
+	/* } */
+
+	/* void updateChi2Unweighted() */
+	/* { */
+	/* 	updateMeanUnweighted(); */
+	/* 	float chi2 = 0; */
+	/* 	maxChi2Itr = timingEvents.begin(); */
+
+	/* 	for(std::vector<EcalTimingEvent>::iterator itr = timingEvents.begin(); */
+	/* 	        itr != timingEvents.end(); ++itr) { */
+	/* 		float singleChi = (itr->time - mean); */
+	/* 		itr->chi2 = singleChi * singleChi; */
+	/* 		chi2 += singleChi * singleChi; */
+
+	/* 		if(itr->chi2 > maxChi2Itr->chi2) { */
+	/* 			maxChi2Itr = itr; */
+	/* 		} */
+	/* 	} */
+
+	/* 	totalChi2 = chi2; */
+	/* } */
+
+	/* void updateMeanWeighted() */
+	/* { */
+	/* 	float meanTmp = 0; */
+	/* 	float mean2Tmp = 0; */
+	/* 	float sigmaTmp = 0; */
+
+	/* 	for(std::vector<EcalTimingEvent>::const_iterator itr = timingEvents.begin(); */
+	/* 	        itr != timingEvents.end(); ++itr) { */
+	/* 		float sigmaT2 = itr->sigmaTime; */
+	/* 		sigmaT2 *= sigmaT2; */
+	/* 		sigmaTmp += 1 / (sigmaT2); */
+	/* 		meanTmp += (itr->time) / (sigmaT2); */
+	/* 		mean2Tmp += ((itr->time) * (itr->time)) / (sigmaT2); */
+	/* 	} */
+
+	/* 	meanE = sqrt(1 / sigmaTmp); */
+	/* 	mean = meanTmp / sigmaTmp; */
+	/* 	rms = sqrt(mean2Tmp / sigmaTmp); */
+	/* 	stdDev = sqrt(rms * rms - mean * mean); */
+	/* } */
+
+	/* void updateMeanUnweighted() */
+	/* { */
+	/* 	float meanTmp = 0; */
+	/* 	float mean2Tmp = 0; */
+
+	/* 	for(std::vector<EcalTimingEvent>::const_iterator itr = timingEvents.begin(); */
+	/* 	        itr != timingEvents.end(); ++itr) { */
+	/* 		meanTmp += itr->time; */
+	/* 		mean2Tmp += (itr->time) * (itr->time); */
+	/* 	} */
+
+	/* 	mean = meanTmp / timingEvents.size(); */
+	/* 	rms = sqrt(mean2Tmp / timingEvents.size()); */
+	/* 	stdDev = sqrt(rms * rms - mean * mean); */
+	/* 	meanE = stdDev / sqrt(timingEvents.size()); // stdDev/sqrt(n) */
+	/* } */
+
 };
