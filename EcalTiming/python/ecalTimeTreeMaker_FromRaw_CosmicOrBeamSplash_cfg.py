efb2bb5b74919f6157cb9007ba42a352d4e3ef48
Shervin <shervin@cern.ch>
2015-04-02 16:32:13 +0200

    modified python

--- EcalTiming/python/ecalTimeTreeMaker_FromRaw_CosmicOrBeamSplash_cfg.py
+++ EcalTiming/python/ecalTimeTreeMaker_FromRaw_CosmicOrBeamSplash_cfg.py
@@ -73,43 +73,56 @@ process.options = cms.untracked.PSet(
 )
 
 # dbs search --query "find file where dataset=/ExpressPhysics/BeamCommissioning09-Express-v2/FEVT and run=124020" | grep store | awk '{printf "\"%s\",\n", $1}'
-process.source = cms.Source(
-    "PoolSource",
-    skipEvents = cms.untracked.uint32(0),
-    fileNames = cms.untracked.vstring(
-    #'file:MyCrab/50988619-41DE-E211-9F98-003048FFD770.root'
-    #'root://xrootd.unl.edu//store/data/Run2010B/Cosmics/RAW/v1/000/144/556/C8B5FCA9-F3B5-DF11-B28A-0030487CD16E.root'
-    #'file:Cosmic-Commissioning2014-Cosmics-RAW-v1-AC4963B3-54BE-E311-97F5-02163E00E6E3.root'
-    #'/store/data/Commissioning2015/Cosmics/RAW-RECO/CosmicSP-6Mar2015-v1/10000/248747E6-25CA-E411-B17C-02163E00BD75.root'
-    #'/store/data/Run2010B/Cosmics/RAW/v1/000/144/559/306A4ABD-F3B5-DF11-9CAD-003048F118C6.root'
-    '/store/data/Commissioning2015/Cosmics/RAW/v1/000/232/881/00000/26ADAFFB-3FAB-E411-A313-02163E011DDC.root'
-     ),               
-    # drop native rechits and clusters, to be sure only those locally made will be picked up
-    inputCommands = cms.untracked.vstring('keep *'
-                                          ,'drop EcalRecHitsSorted_*_*_RECO' # drop hfRecoEcalCandidate as remade in this process
-                                          , 'drop recoSuperClusters_*_*_RECO' # drop hfRecoEcalCandidate as remade in this process
-                                          , 'drop recoCaloClusters_*_*_RECO'
-                                          )
+# Input source
+process.source = cms.Source("PoolSource",
+    secondaryFileNames = cms.untracked.vstring(),
+                            #  fileNames = cms.untracked.vstring('file:test_DIGI.root')
+  fileNames = cms.untracked.vstring(
+        #'file:FE13B243-C0D8-E411-ADAC-02163E011D52.root')
+        '/store/data/Commissioning2015/Cosmics/RAW/v1/000/239/517/00000/FE13B243-C0D8-E411-ADAC-02163E011D52.root')
+)
+
+# process.source = cms.Source(
+#     "PoolSource",
+#     skipEvents = cms.untracked.uint32(0),
+#     fileNames = cms.untracked.vstring(
+#     #'file:MyCrab/50988619-41DE-E211-9F98-003048FFD770.root'
+#     #'root://xrootd.unl.edu//store/data/Run2010B/Cosmics/RAW/v1/000/144/556/C8B5FCA9-F3B5-DF11-B28A-0030487CD16E.root'
+#     #'file:Cosmic-Commissioning2014-Cosmics-RAW-v1-AC4963B3-54BE-E311-97F5-02163E00E6E3.root'
+#     #'/store/data/Commissioning2015/Cosmics/RAW-RECO/CosmicSP-6Mar2015-v1/10000/248747E6-25CA-E411-B17C-02163E00BD75.root'
+#     #'/store/data/Run2010B/Cosmics/RAW/v1/000/144/559/306A4ABD-F3B5-DF11-9CAD-003048F118C6.root'
+#     '/store/data/Commissioning2015/Cosmics/RAW/v1/000/232/881/00000/26ADAFFB-3FAB-E411-A313-02163E011DDC.root'
+#      ),               
+#     # drop native rechits and clusters, to be sure only those locally made will be picked up
+#     inputCommands = cms.untracked.vstring('keep *'
+#                                           ,'drop EcalRecHitsSorted_*_*_RECO' # drop hfRecoEcalCandidate as remade in this process
+#                                           , 'drop recoSuperClusters_*_*_RECO' # drop hfRecoEcalCandidate as remade in this process
+#                                           , 'drop recoCaloClusters_*_*_RECO'
+#                                           )
 
 
-    )
+#     )
 
 ## Dumpevent Event Contents
 process.dumpEvContent = cms.EDAnalyzer("EventContentAnalyzer")
 
 ### NumBer of events
-process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(100))
 
 
 ### Process Full Path
-process.p = cms.Path( process.spashesHltFilter    
-                     + process.preScaler 
-                     + process.digiStep 
+process.p = cms.Path( #process.spashesHltFilter    
+                     #+ process.preScaler 
+                      process.digiStep 
                      #+ process.muonSequence 
                      + process.caloCosmicOrSplashRECOSequence 
-                + process.ecalTimeTree
                     )
 
 
 ### Schedule ###
 process.schedule = cms.Schedule(process.p) 
+
+process.looper = cms.Looper("EcalTimingCalibProducer",
+                            recHitEBCollection = cms.InputTag("ecalRecHit","EcalRecHitsEB"),
+                            recHitEECollection = cms.InputTag("ecalRecHit","EcalRecHitsEE"),
+                            )
